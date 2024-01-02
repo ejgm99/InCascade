7a86c0971b228f1e5c340d6d9087d821936b84f7
ejgm99 <erickjgomez99@gmail.com>
2024-01-01 23:30:45 -0600

    editing title and description

--- src/data/fetch.js
+++ src/data/fetch.js
@@ -1,13 +1,14 @@
 import { db } from '../data/firebase.js';
 
 export async function getPost(post_id) {
-    // Get the Firestore document by ID
-    try {
-        const docSnapshot = await db.doc(`posts/${post_id}`).get();
-        return docSnapshot.data();
-    } catch (err) {
-        console.log(err)
-    }
-    // Access the child element by its property name
-    // console.log(docSnapshot.data())
+  // Get the Firestore document by ID
+  try {
+    const docSnapshot = await db.doc(`posts/${post_id}`).get();
+    console.log(docSnapshot)
+    return docSnapshot.data();
+  } catch (err) {
+    console.log(err)
+  }
+  // Access the child element by its property name
+  // console.log(docSnapshot.data())
 }
--- src/data/write.js
+++ src/data/write.js
@@ -1,13 +1,12 @@
 import { db } from '../data/firebase.js';
 
 export function writeDataToMapField(docId, cascade_map) {
-  console.log(cascade_map)
-    
-    db.collection('posts').doc(docId).update(cascade_map)
-    .then(() => {
-      console.log('Data written to map field successfully!');
-    })
-    .catch((error) => {
-      console.error('Error writing data to map field:', error);
-    });
-  }
+  db.collection('posts').doc(docId).update(cascade_map)
+}
+
+
+export function updateDoc(docId, docKey, new_title) {
+  let update_map = {}
+  update_map[docKey] = new_title;
+  db.collection('posts').doc(docId).update(update_map)
+}
